4e3c5fb24a573f3b98eaaa2578a423af3c48f37f
Martin Kong Morales <123519281+martinkongm@users.noreply.github.com>
2023-06-30 19:38:52 -0500

    Delete com/gestioncitas directory

--- com/gestioncitas/interfaces/DAOHorario.java
+++
@@ -1,7 +0,0 @@
-package com.gestioncitas.interfaces;
-
-import javax.swing.JTable;
-
-public interface DAOHorario {
-    public void mostrar(JTable vista) throws Exception;
-}
--- com/gestioncitas/interfaces/DAOMedico.java
+++
@@ -1,7 +0,0 @@
-package com.gestioncitas.interfaces;
-
-import javax.swing.JTable;
-
-public interface DAOMedico {
-    public void mostrar(JTable vista) throws Exception;
-}
